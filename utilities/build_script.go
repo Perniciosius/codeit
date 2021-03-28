975b7e14aaf7cd954462ab566df6fcff3dc45f58
Aman <aksinha.101010@gmail.com>
2021-03-28 17:52:28 +0530

    take input from input.txt

--- utilities/build_script.go
+++ utilities/build_script.go
@@ -8,7 +8,8 @@ func BuildScript(compileCommand string, executeCommand string, timeout int) []by
 	}
 	return []byte("#!/bin/bash\n" +
 		compileCommand +
-		"timeout -k 0.5s " + strconv.Itoa(timeout) + "s " + executeCommand + " > output.txt\n" +
+		"timeout -k 0.5s " + strconv.Itoa(timeout) + "s " +
+		executeCommand + " < input.txt > output.txt\n" +
 		"if [[ $? == 124 || $? == 137 ]]\n" +
 		"then\n" +
 		"    echo \"timeout: Program execution terminated.\nPlease check for infinite loop.\"\n" +
