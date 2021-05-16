03cdf4b552c3704ef3b848c9a46b26321e672ea1
Pratyush2k1 <praty2k1@gmail.com>
2021-05-17 00:31:02 +0530

    download button for code

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,5 +1,6 @@
+let downloadCode
 $(function () {
-    CodeMirror.fromTextArea(document.getElementById("codemirror-textarea"), {
+    let editor = CodeMirror.fromTextArea(document.getElementById("codemirror-textarea"), {
         lineNumbers: true,
         mode: "python",
         theme: "material",
@@ -7,6 +8,46 @@ $(function () {
         matchBrackets: true,
         smartIndent: true,
     })
+    downloadCode = function () {
+        let textval = editor.getValue()
+        textval = textval.replace(/\n/g, "\r\n")
+        let lang = $("#language").val()
+        let textFileAsBlob = new Blob([textval], { type: 'text/HTML' })
+        let filename = ''
+        switch (lang) {
+            case "c":
+                filename = "code.c"
+                break
+            case "c++":
+                filename = "code.cpp"
+                break
+            case "java":
+                filename = "code.java"
+                break
+            case "python2":
+            case "python3":
+                filename = "code.py"
+                break
+            case "go":
+                filename = "code.go"
+                break
+            case "javascript":
+                filename = "code.js"
+                break
+            case "typescript":
+                filename = "code.ts"
+                break
+        }
+        let downloadLink = document.createElement("a");
+        downloadLink.download = filename;
+        downloadLink.innerHTML = "LINKTITLE";
+        window.URL = window.URL || window.webkitURL;
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+    }
     $("form").submit(function (event) {
         let lang = $("#language").val()
         event.preventDefault()
@@ -17,3 +58,5 @@ $(function () {
         })
     })
 })
+
+
