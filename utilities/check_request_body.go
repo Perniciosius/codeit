9257264fc01b2746ecc3cbf3ae24585dc229b506
Aman <aksinha.101010@gmail.com>
2021-05-21 12:35:04 +0530

    change response json key

--- utilities/check_request_body.go
+++ utilities/check_request_body.go
@@ -9,7 +9,7 @@ func CheckRequestBody(ctx *fiber.Ctx) error {
 	body := new(model.CompileRequestBody)
 	if err := ctx.BodyParser(body); err != nil || body.Code == "" {
 		return ctx.JSON(map[string]string{
-			"error": "Empty program",
+			"output": "Empty program",
 		})
 	}
 	return ctx.Next()
